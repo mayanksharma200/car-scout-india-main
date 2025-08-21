5def0b85417a25fe4c18735bc4bf26e2ef76a2f1
Mayank <vashistmayankk@gmail.com>
2025-08-21 06:23:26 +0530

    add options in the search filters

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -238,8 +238,8 @@ const HeroSection = () => {
 
   const filterOptions = {
     fuelTypes: ["Petrol", "Diesel", "Electric", "Hybrid", "CNG"],
-    transmissions: ["Manual", "Automatic", "CVT"],
-    bodyTypes: ["Hatchback", "Sedan", "SUV", "Coupe", "Convertible"],
+    transmissions: ["Manual", "Automatic", "CVT","AMT"],
+    bodyTypes: ["Hatchback", "Sedan", "SUV", "Coupe", "Convertible", "MPV"],
     seatingOptions: ["2", "4", "5", "7", "8+"],
     brands: [
       "Maruti",
