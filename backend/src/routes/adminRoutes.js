be3d74dd33040f105f5584326deb348b32240b37
Mayank <vashistmayankk@gmail.com>
2025-08-26 14:35:46 +0530

    Revert "structured backend implementation"
    This reverts commit 7e43655a520f7872e32d4e45641dd097a6f364f5.

--- backend/src/routes/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const adminController = require('../controllers/adminController');
-const { validateToken, requireAdmin } = require('../middleware/auth');
-
-// Admin routes (require authentication and admin role)
-router.get('/stats', validateToken, requireAdmin, adminController.getStats);
-
-module.exports = router;
